256d8022119881efd13d3df274c5aae7902ee4b7
Dee Reddy <deesus@pm.me>
2022-02-02 02:37:34 -0800

    Feat: write scraped data to csv file.

--- scrape_linuxquestions.py
+++ scrape_linuxquestions.py
@@ -1,13 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+import csv
 
 __author__ = 'Deepankara Reddy'
 
 
 # ---------- constants: ----------
-SCRAPED_DATA_PATH = './scraped_data'
 BASE_URL = 'https://www.linuxquestions.org/questions/linux-software-2/'
+CSV_COL_NAMES = ['title', 'description', 'url', 'date']
+
+
+# ---------- functions: ----------
+def write_to_file(rows):
+    with open('scraped_questions.csv', 'w') as file:
+        writer = csv.writer(file)
+        writer.writerow(CSV_COL_NAMES)
+        writer.writerows(rows)
 
 
 # ---------- main: ----------
@@ -25,6 +34,8 @@ if __name__ == '__main__':
     threads = driver.find_elements(By.CSS_SELECTOR, '#threadslist a[id^="thread_title"]')
     links = [(thread.get_attribute('href'), thread.text) for thread in threads]
 
+    # scrape every thread on the page:
+    data = []
     for url, title in links:
         driver.get(url)
         WebDriverWait(driver, timeout=5).until(lambda d: d.find_element(By.ID, 'posts'))
@@ -32,10 +43,10 @@ if __name__ == '__main__':
         post = driver.find_element(By.CSS_SELECTOR, '#posts table[id^="post"]')
         description = post.find_element(By.CSS_SELECTOR, 'div[id^="post_message"]').text
         date = post.find_element(By.CSS_SELECTOR, '.thead').text
-
-        print("title:", title, "description:", description, "url:", url, "date:", date, sep="\n\n")
-        print('=======================')
+        data.append([title, description, url, date])
 
         driver.back()
+
+    write_to_file(data)
 
     driver.quit()
